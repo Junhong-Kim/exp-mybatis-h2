c4dc983eb34ddc7a2145fbdfd70df2ce81d91093
Junhong-Kim <jhk1005jhk@gmail.com>
2020-09-25 23:56:29 +0900

    init

---
+++ src/main/java/kim/junhong/expmybatish2/ExpMybatisH2Application.java
@@ -0,0 +1,13 @@
+package kim.junhong.expmybatish2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpMybatisH2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExpMybatisH2Application.class, args);
+    }
+
+}
---
+++ src/test/java/kim/junhong/expmybatish2/ExpMybatisH2ApplicationTests.java
@@ -0,0 +1,13 @@
+package kim.junhong.expmybatish2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpMybatisH2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
